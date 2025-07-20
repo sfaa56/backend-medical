80579c81fb53daa3b45ed7108fef8f3145115bcf
sfaa <ym6772003@gmail.com>
2025-07-20 16:39:51 -0700

    db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDb = async () => {
-  try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log(
-      "Database connected:",
-      connect.connection.host,
-      connect.connection.db.databaseName // <-- FIXED
-    );
-  } catch (err) {
-    console.log("Database connection failed:", err.message);
-    process.exit(1);
-  }
-};
+const connectDb = async ()=>{
+    try{
+        const connect =await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log("Database connected:",connect.connection.host, connect.connection.name);
+
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
 
 module.exports = connectDb;
